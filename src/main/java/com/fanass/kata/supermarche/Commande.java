08ac26bc824ab38986ac46762c3362dc660870b6
Lenovo <anass.fetal0@gmail.com>
2020-07-28 06:39:35 +0200

    ajout du code pour faire passer le 2eme cas de test:tester avec un seul produit au panier

--- src/main/java/com/fanass/kata/supermarche/Commande.java
+++ src/main/java/com/fanass/kata/supermarche/Commande.java
@@ -2,12 +2,19 @@ package com.fanass.kata.supermarche;
 
 import java.math.BigDecimal;
 
+import com.fanass.kata.supermarche.model.GroupProduit;
 import com.fanass.kata.supermarche.model.Panier;
 
 public class Commande {
 
 	public BigDecimal total(Panier panier) {
-
+		GroupProduit gProduit;
+		if(panier.getListGroupProduit().size()!=0) {
+			gProduit = new GroupProduit();
+			gProduit = panier.getListGroupProduit().get(0);
+			
+			return gProduit.getProduit().getPrix().getPrix(); 
+		}
 		return intToBigDecimal(0);
 	}
 	
